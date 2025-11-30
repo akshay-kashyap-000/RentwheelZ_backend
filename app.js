652c9167b277b5ee38f40a7c235e6b53e0a5dc8b
akshay-kashyap-000 <akshaykashyap265@gmail.com>
2025-12-01 00:00:56 +0530

    added cors

--- app.js
+++ app.js
@@ -9,10 +9,16 @@ const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
 const mapRoutes = require('./routes/maps.routes');
 const rideRoutes = require('./routes/ride.routes');
-
+const cors = require('cors');
 connectToDb();
 
-app.use(cors());
+app.use(cors({
+    // allow both Vite (5173) and CRA (3000) dev servers during development
+    origin:["http://localhost:5173", "http://localhost:3000"], //$ defines the allowed origins
+    credentials:true, //$ to allow cookies to be sent to frontend along with requests
+    methods:["GET","POST","PATCH","DELETE"] //$ allowed methods
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
